12ff5688b8dc9bfcbd440da143c70bfa48f64e74
Sung Paik <sungypaik@gmail.com>
2023-09-19 23:04:57 -0400

    handle scroll

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,13 @@ import { CustomButton } from '.';
 type Props = {};
 
 const Hero = (props: Props) => {
-  const handleScroll = () => {};
+  const handleScroll = () => {
+    const nextSection = document.getElementById('discover');
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   return (
     <div className='hero'>
